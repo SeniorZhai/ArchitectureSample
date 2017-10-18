64dc75b5295a8790226352a7f716ddf4a462c084
zhaitao <370985116@qq.com>
2017-10-18 17:28:41 +0800

    remove old code

--- app/src/main/java/io/github/seniorzhai/architecturesample/network/IP.java
+++
@@ -1,9 +0,0 @@
-package io.github.seniorzhai.architecturesample.network;
-
-/**
- * Created by zhai on 2017/10/17.
- */
-
-public class IP {
-    public String origin;
-}
---
+++ app/src/main/java/io/github/seniorzhai/architecturesample/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.github.seniorzhai.architecturesample.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.github.seniorzhai.architecturesample.R
+import io.github.seniorzhai.architecturesample.ui.search.SearchFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
